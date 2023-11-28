b108853b1db9aa8771620cfeb4eeb30e18441c86
TienLoFi <ngoctiendz2k3@gmail.com>
2023-11-28 17:11:13 +0700

    update-ui-header

--- Hangoctien2121110200_cdtt/webbanxe/src/components/HeaderComponent/HeaderComponent.jsx
+++ Hangoctien2121110200_cdtt/webbanxe/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import { Col } from "antd";
+import { WrapperHeader, WrapperHeaderAcount, WrapperTextHeader,WrapperTextHeaderSmall } from "./style";
+// import Search from "antd/es/input/Search";
+import { UserOutlined, CaretDownOutlined ,ShoppingCartOutlined} from "@ant-design/icons";
+import ButttonInputSearch from "../ButtonInputSearch/ButttonInputSearch";
 const HeaderComponent = () => {
   return (
     <div>
-      HeaderComponent
+      <WrapperHeader gutter={16}>
+        <Col span={6}>
+          <WrapperTextHeader> T-Supper</WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+        <ButttonInputSearch
+              size="large"
+              bordered={false}
+              textbutton="Tìm kiếm"
+              placeholder="input search text"
+             // onChange={onSearch}
+              backgroundColorButton="#5a20c1"
+            />
+        </Col>
+        <Col span={6} style={{display:"flex",gap:"25px"}}>
+          <WrapperHeaderAcount >
+          <UserOutlined style={{ fontSize:'30px'}} />
+          <div>
+            
+            <div>
+             <WrapperTextHeaderSmall>Đăng Nhập Đăng Kí</WrapperTextHeaderSmall>
+            </div>
+            <WrapperTextHeaderSmall>Tài Khoản</WrapperTextHeaderSmall>
+            <CaretDownOutlined />
+          </div>
+          </WrapperHeaderAcount>
+      <div>
+      <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
+      <WrapperTextHeaderSmall>Giỏ Hàng </WrapperTextHeaderSmall>
+      </div>
+        </Col>
+      </WrapperHeader>
     </div>
-  ) 
-}
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
---
+++ Hangoctien2121110200_cdtt/webbanxe/src/components/HeaderComponent/style.js
@@ -0,0 +1,25 @@
+import {Row} from "antd";
+import{styled} from "styled-components"
+
+
+export const WrapperHeader= styled(Row)`
+padding:10px 120px;
+background-color:rgb(26,128,255);
+align-items: center
+
+`
+export const WrapperTextHeader =styled.span`
+font-size:20px;
+color:#fff;
+font-weight:bold;
+text-align:left`
+export const WrapperHeaderAcount=   styled.div`
+color:#fff;
+display:flex;
+align-items:center;
+font-size:12px;
+gap: 10px;
+`    
+export const WrapperTextHeaderSmall=styled.span`
+font-size:12px;
+color:#fff;`
